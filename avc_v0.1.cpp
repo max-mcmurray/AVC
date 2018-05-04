a06b3f525d1dc3263b584f90c72925fbef69ddcd
joshuajhindley <38927613+joshuajhindley@users.noreply.github.com>
2018-05-04 13:41:50 +1200

    Add files via upload

---
+++ avc_v0.1.cpp
@@ -0,0 +1,37 @@
+//This program is the intellectual property of TriTeam.
+//This program was programmed by Joshua Hindley, Ben Robertson, Max McMurray, Penny Peneueta and Toby Stayner
+
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+//main method. determines which method to call
+void main()
+{
+	init();
+}
+
+//opens the gate at the start of the course, completing the first quadrant
+void openStartGate()
+{
+}
+
+//follows the curvy white line, completing the second quadrant
+void followLine()
+{
+}
+
+//follows the line to solve the line maze, completing the third quadrant
+void navigateLineMaze()
+{
+}
+
+//follows the left/right wall of the maze, completing the final quadrant
+void navigateWalledMaze()
+{
+}
+
+//waits for the gate in the walled maze to open and goes through it when does
+void waitForGateToOpen()
+{
+}
